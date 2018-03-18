899f8ccc5bafc5e50b503f34dc3f210944abd1e0
sarbesh <sarbeshsarkarsks@gmail.com>
2018-03-19 01:32:35 +0530

    after templates

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 
 from . import views
 urlpatterns = [
+    path('hello', views.hello, name='hello'),
     path('', views.index, name='index'),
 ]
--- portfolio/views.py
+++ portfolio/views.py
@@ -3,4 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
+    return render('base.html',)
+
+def hello(request):
     return HttpResponse("<h1>This is my portfolio page</h1>")
