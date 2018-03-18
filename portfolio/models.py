b9e82a5197676ee4a9c465f968144360fb7d35a2
sarbesh <sarbeshsarkarsks@gmail.com>
2018-03-18 20:50:37 +0530

    after model

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 
 # Create your models here.
-#class Name(models.Model):
-#    College = models.CharField(max_lenght=250)
-#    Photo = models.CharField(max_lenght=1000)
-#    Email = models.CharField(max_lenght=250)
-#    phone = models.IntegerField(max_lenght=12)
-#    linkedin = models.URLField(max_lenght=100)
-#    github = models.URLField(max_lenght=100)
+class Name(models.Model):
+#    question_text = models.CharField(max_length=200)
+    College = models.CharField(max_length=250)
+    Photo = models.CharField(max_length=1000)
+    Email = models.CharField(max_length=250)
+    phone = models.IntegerField()
+    linkedin = models.URLField(max_length=100)
+    github = models.URLField(max_length=100)
