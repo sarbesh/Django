8a4b732158f10aabafcc59703a6491eb70e4fad2
sarbesh <sarbeshsarkarsks@gmail.com>
2018-03-18 20:42:34 +0530

    after model

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+#class Name(models.Model):
+#    College = models.CharField(max_lenght=250)
+#    Photo = models.CharField(max_lenght=1000)
+#    Email = models.CharField(max_lenght=250)
+#    phone = models.IntegerField(max_lenght=12)
+#    linkedin = models.URLField(max_lenght=100)
+#    github = models.URLField(max_lenght=100)
