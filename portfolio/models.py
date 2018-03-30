c1a46c02be68e7ee2b602005eb9af17f596bf296
sarbesh <sarbeshsarkarsks@gmail.com>
2018-03-30 18:59:18 +0530

    portfolio test models

--- portfolio/models.py
+++ portfolio/models.py
@@ -3,9 +3,14 @@ from django.db import models
 # Create your models here.
 class Name(models.Model):
 #    question_text = models.CharField(max_length=200)
+    First_name = models.CharField(max_length=100)
+    Middle_name = models.CharField(max_length=100)
+    Last_name = models.CharField(max_length=100)
+    Email = models.EmailField(default="", max_length=250, primary_key=True)
+    User = models.CharField(max_length=250)
     College = models.CharField(max_length=250)
-    Photo = models.CharField(max_length=1000)
-    Email = models.CharField(max_length=250)
+    DOB = models.DateField('Birth date')
+    #Photo =StdImageField(upload_to='user/Image/', variations={'thumbnail': (150,200,True)})
     phone = models.IntegerField()
-    linkedin = models.URLField(max_length=100)
-    github = models.URLField(max_length=100)
+    linkedin = models.URLField(max_length=100, blank = False)
+    github = models.URLField(max_length=100, blank = False)
